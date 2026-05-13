33675855a8f61ebddc9c9e85ad5fafce0a36be3b
dhiyasaminathan-allen <dhiyasaminathan13@gmail.com>
2026-05-13 14:43:56 +0530

    Update Mypython.py

--- Mypython.py
+++ Mypython.py
@@ -3,3 +3,11 @@ name="Sandhiya";
 print("Hello I am " + name);
 print("Day 1: Complete");
 print("Day 2: Starting");
+
+
+#Calculate BMI value
+print("BMI Claculator")
+height = float(input("Enter your Height: "))
+weight = float(input("Enter your Weight: "))
+bmi = height / weight
+print("Your BMI value is: ",bmi)
