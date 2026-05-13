8952e31516ce59f9b6e4faa60e95c27bc8925aef
dhiyasaminathan-allen <dhiyasaminathan13@gmail.com>
2026-05-13 14:42:19 +0530

    Create project2.py

---
+++ project2.py
@@ -0,0 +1,6 @@
+#Calculate BMI value
+print("BMI Claculator")
+height = float(input("Enter your Height: "))
+weight = float(input("Enter your Weight: "))
+bmi = height / weight
+print("Your BMI value is: ",bmi)
